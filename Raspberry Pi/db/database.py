d177cadbe9689dc42f89114590f35097e5496535
nguyenmthien <nguyenmthien@live.com>
2019-09-25 22:46:22 +0700

    Create SQLite OOP version

--- Raspberry Pi/db/database.py
+++ Raspberry Pi/db/database.py
@@ -1,6 +1,36 @@
+#!/usr/bin/env python3
+# *_* coding: utf-8 *_*
+
+"""Contain functions to create and manipulate a db"""
+
+
 import sqlite3
+import time
 
 def createdb(mode, name):
+    """If mode == "thermo", create table with name in for data from sensors"""
     if mode == "thermo":
-        connection = sqlite3.connect("myTable.db") 
-        crsr = connection.cursor() 
+        connection = sqlite3.connect(name) 
+        crsr = connection.cursor()
+        crsr.execute("""CREATE TABLE IF NOT EXISTS therm (  
+        time INTEGER ,
+        sensorname INTEGER,  
+        temperature REAL,  
+        humidity REAL);""")
+        connection.commit()
+        connection.close()
+
+
+def writetherm(db_name, sensor_name, temp, humid):
+    """Write the temperature and humidity to db"""
+    connection = sqlite3.connect(db_name) 
+    crsr = connection.cursor() 
+    current_time = int(time.time())
+    crsr.execute("INSERT INTO therm VALUES (?, ?, ?, ?)",
+        (current_time, sensor_name, temp, humid) )
+    connection.commit()
+    connection.close()
+
+if __name__ == "__main__": 
+    createdb("thermo","my.db")
+    writetherm("my.db",1,23.3,12.3)
