148df328751535749d8c0cb8c5342cc75c894058
Huy Le <lehuutuanhuy@gmail.com>
2019-09-27 13:15:38 +0700

    TCP/IP Server

--- Raspberry Pi/TCP/main.py
+++ Raspberry Pi/TCP/main.py
@@ -1,16 +1,42 @@
 import socket_server
 import time
 
-test = socket_server.tcp_server("192.168.1.7", 2033)
+vguserver = socket_server.tcp_server("192.168.1.4", 2033)
+last_t = time.time()
 
 while True:
-    
-    test.create_sockets()
+    vguserver.update_sockets_list()
+    try:
+        vguserver.check_read_sockets()
+    except:
+        id = input("Enter ID: ")
+        vguserver.new_socket_handle(id)    
+    message_list = vguserver.recvall()
+    if message_list != []:
+        print(message_list)
+    vguserver.sendall("abc")
+    '''t = time.time()
+    if (t - last_t) > 3:
+        vguserver.sendall("abc")
+        last_t = t'''
+
+
+
+
+
+
+
+
+    '''test.create_sockets()
     client_num = int(input("Enter the client number: "))
     test.create_sockets()
     try:
         test.sendmsg("haha", client_num)
         time.sleep(1)
         test.recvmsg(client_num)
+
+        s= time.time()
+
     except:
         print("The client does not exist!")
+        socket_server.tcp_server()'''
--- Raspberry Pi/TCP/socket_server.py
+++ Raspberry Pi/TCP/socket_server.py
@@ -10,8 +10,8 @@ import time
 def receive_message(client_socket):
     """Does this function needed?, if yes, please put a docstring in here"""
     try:
-        mess = client_socket.recv(10)
-        if not len(mess):
+        mess = client_socket.recv(128)
+        if (not len(mess)) or (len(mess) <= 2):
             return False
         return mess
     
@@ -19,7 +19,7 @@ def receive_message(client_socket):
         return False
 
 class tcp_server:
-    """This is a docstring"""
+    """Create a TCP/IP Server"""
     count = 1
     #HEADER_LENGTH = 10
     def __init__(self,IP,PORT):
@@ -30,29 +30,71 @@ class tcp_server:
         self.server_socket.bind((self.IP, self.PORT))
         self.server_socket.listen()
         self.sockets_list = [self.server_socket]
-        self.clients = {}
+        self.id_dict = {}
         self.msg = {}
+        self.read_sockets = []
+        self.write_sockets = []
+        self.exception_sockets = []
         
-    def create_sockets(self):
-        read_sockets, _, exception_sockets = select.select(self.sockets_list, [], self.sockets_list)
-        
-        for notified_socket in read_sockets:
+
+    def update_sockets_list(self):
+        """Update sockets list to read_sockets, write_sockets, exception_sockets"""
+        self.read_sockets = []
+        self.write_sockets = []
+        self.exception_sockets = []
+        self.read_sockets, self.write_sockets, self.exception_sockets = select.select(self.sockets_list, [], self.sockets_list)
+
+    def check_read_sockets(self):
+        """Handle new connection after updating socket lists"""    
+        for notified_socket in self.read_sockets:
             if notified_socket == self.server_socket:
-        
-                client_socket, client_address = self.server_socket.accept()
-                self.sockets_list.append(client_socket)
-                self.clients[client_socket] = tcp_server.count
-                tcp_server.count += 1
-                #print(self.sockets_list)
-                
-        for notified_socket in exception_sockets:
+                raise Exception('New Connection')
+
+    def new_socket_handle(self, id):
+        client_socket, client_address = self.server_socket.accept()
+        self.sockets_list.append(client_socket)
+        #self.clients[client_socket] = tcp_server.count
+        #tcp_server.count += 1
+        self.id_dict[client_socket] = id
+        #print(self.sockets_list)
+
+        '''for notified_socket in self.exception_sockets:
             self.sockets_list.remove(notified_socket)
-            del self.clients[notified_socket]
+            del self.clients[notified_socket]'''
             
-    def sendmsg(self, mess, num):
-        
-        self.sockets_list[num].send(mess.encode('utf-8'))
-        
+    def sendmsg(self, mess, id):
+        """Send a message to a specific client"""
+        #self..send(mess.encode('utf-8'))
+    
+    def sendall(self, mess):
+        for key in self.id_dict:
+            if  (self.id_dict[key] != 'UPS') and (self.id_dict[key] != 'AC'):
+                key.send(mess.encode('utf-8'))
+
+    def therm_parsing(self, mess):
+        """Split a message from a client into 2 variables"""
+        mess_list = mess.split()
+        if len(mess_list) == 2:
+            return mess_list[0], mess_list[1]
+
+    def recvall(self):
+        """Receive all messages from clients"""
+        return_list = []
+        for notified_socket in self.read_sockets:
+            if notified_socket != self.server_socket:
+                if self.id_dict[notified_socket] != 'UPS':
+                    mess_dict = {'ID':self.id_dict[notified_socket]}
+                    message = receive_message(notified_socket)
+                    if message is False:
+                        self.sockets_list.remove(notified_socket)
+                        continue
+                    temp, humid = self.therm_parsing(message)
+                    mess_dict['Temp'] = temp.decode('utf-8')
+                    mess_dict['Humid'] = humid.decode('utf-8')
+                    return_list.append(mess_dict)
+
+        return return_list
+
     def recvmsg(self, num):
         message = receive_message(self.sockets_list[num])
         if message is False:
