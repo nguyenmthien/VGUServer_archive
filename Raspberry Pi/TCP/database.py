bd37cb96774ff283fc7916fb49d283075ba2d149
nguyenmthien <nguyenmthien@live.com>
2019-10-01 21:25:38 +0700

    create user mode

--- Raspberry Pi/TCP/database.py
+++ Raspberry Pi/TCP/database.py
@@ -10,7 +10,8 @@ import sqlite3
 import time
 
 def createdb(mode, name):
-    """If mode == "thermo", create table with name for data from sensors"""
+    """If mode == "thermo", create table with name for data from sensors
+    mode == user, create table with username & password"""
     if mode == "thermo":
         connection = sqlite3.connect(name) 
         crsr = connection.cursor()
@@ -19,6 +20,15 @@ def createdb(mode, name):
         sensorname INTEGER,  
         temperature REAL,  
         humidity REAL);""")
+        connection.commit()
+        connection.close()
+    if mode == "user":
+        connection = sqlite3.connect(name) 
+        crsr = connection.cursor()
+        crsr.execute("""CREATE TABLE IF NOT EXISTS user (  
+        time INTEGER ,
+        username INTEGER,  
+        password INTEGER);""")
         connection.commit()
         connection.close()
 
@@ -33,6 +43,19 @@ def writetherm(db_name, sensor_name, temp, humid):
     connection.commit()
     connection.close()
 
+def writeuser(db_name, username, password):
+    """Write the username and [humidity] to db"""
+    connection = sqlite3.connect(db_name) 
+    crsr = connection.cursor() 
+    current_time = int(time.time())
+    crsr.execute("INSERT INTO user VALUES (?, ?, ?)",
+        (current_time, username, password) )
+    connection.commit()
+    connection.close()
+
 if __name__ == "__main__": 
-    createdb("thermo","my.db")
-    writetherm("my.db",1,23.3,12.3)
+    #createdb("thermo","my.db")
+    #writetherm("my.db",1,23.3,12.3)
+    createdb("user","username.db")
+    writeuser("username.db","abc","xyz")
+
