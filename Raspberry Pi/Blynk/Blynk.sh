b1b20e6d32cf151d5328b8453e33ab65e91f8d46
nguyenmthien <nguyenmthien@live.com>
2019-11-21 10:41:25 +0700

    changed bash script

--- Raspberry Pi/Blynk/Blynk.sh
+++ Raspberry Pi/Blynk/Blynk.sh
@@ -1,2 +1,2 @@
-cd /home/pi/Blynk
-java -jar server-0.41.11.java8.jar -dataFolder /home/pi/Blynk/logs
+#!/usr/bin/env bash
+java -jar /home/pi/Blynk/server-0.41.11-java8.jar -dataFolder /home/pi/Blynk/logs
