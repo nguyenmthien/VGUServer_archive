e1c8abd76243303466c886d6fd5256e514f379bd
nguyenmthien <nguyenmthien@live.com>
2019-11-27 22:40:38 +0700

    merge

--- Raspberry Pi/TCP/socket_server.py
+++ Raspberry Pi/TCP/socket_server.py
@@ -17,6 +17,19 @@ class address_does_not_exist(Exception):
     def __init__(self, *args):
         super().__init__(*args)
 
+def get_ip():
+    """Find local IP of the current network interface, avoid 127.0.0.1"""
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
 
 def receive_message(client_socket):
     """Recive message from client_socket"""
