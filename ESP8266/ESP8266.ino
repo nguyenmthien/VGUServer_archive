2e57459e8ac2bb3c6557c584fba9bd90c0d03745
nguyenmthien <nguyenmthien@live.com>
2019-11-28 09:35:09 +0700

    initialization

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -1,77 +1,39 @@
 #include <ESP8266WiFi.h>
 #include <Wire.h>
 
-const char* ssid     = "Tran gia phuc";
-const char* password = "phm123456";
+#ifndef STASSID
+#define STASSID "nguyenmthien"
+#define STAPSK  "299792458"
+#define SLEEPTIME 10e6 //in microseconds
+#define I2C_MASTER 0x02
+#define I2C_SLAVE 0x40
+#endif
 
-const int16_t I2C_MASTER = 0x02;
-const int16_t I2C_SLAVE = 0x40;
+const char* ssid     = STASSID;
+const char* password = STAPSK;
 
-WiFiClient client;
-const int port = 2033;
-const char* host = "192.168.1.4";
+IPAddress ip( 192, 168, 0, 128 );
+IPAddress gateway( 192, 168, 0, 1 );
+IPAddress subnet( 255, 255, 255, 0 );
 
-void setupi2c();
-void setupWiFi();
-float readi2c(int mode);
-void sendWiFi(int msg);
+const char* host = "192.168.0.102";
+const uint16_t port = 2033;
 
-void setup()
-{
-    setupi2c();
-    setupWiFi();
-}
-
-void loop()
-{
-    if (client.available())
-    {
-        while (client.available())
-        {
-            client.read();
-        }
-        float temp = readi2c(0xF3);
-        float humid = readi2c(0xF5);
-        String message = "";
-        message = String(temp, 4) + " " + String(humid, 2);
-        client.print(message);
-    }
-}
-
-void setupi2c()
-{
+void setupi2c() {
     Wire.pins(0, 2); 
     Wire.begin();
     Wire.beginTransmission(I2C_SLAVE);
     Wire.endTransmission();
-    delay(300);
+    delay(3);
 }
 
-
-
-void setupWiFi()
-{
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-    delay(500);
-    }
-
-    delay(5000);
-    
-    client.connect(host, port);
-
-}
-
-float readi2c(int mode)
-{
+float readi2c (int mode) {
   unsigned int data[2];
   Wire.beginTransmission(I2C_SLAVE);
   //Send humidity measurement command
   Wire.write(mode);
   Wire.endTransmission();
-  delay(500);
+  delay(50);
  
   // Request 2 bytes of data
   Wire.requestFrom(I2C_SLAVE, 2);
@@ -82,7 +44,7 @@ float readi2c(int mode)
     data[1] = Wire.read();
   }
   
-  // Convert the data for humidity
+  // Convert the data for humidity mode
   if (mode == 0xF5)
   {
       float humidity  = ((data[0] * 256.0) + data[1]);
@@ -90,10 +52,72 @@ float readi2c(int mode)
       return humidity;  
   }
   
-  if (mode==0xF3)
+  // Convert the data for temperature mode
+  if (mode == 0xF3)
   {
       float temp  = ((data[0] * 256.0) + data[1]);
       float celsTemp = ((175.72 * temp) / 65536.0) - 46.85;
       return celsTemp;
   }
 } 
+
+void setup() {
+    Serial.begin(115200);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.mode(WIFI_STA);    
+    WiFi.config( ip, gateway, subnet );
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  Serial.print("connecting to ");
+  Serial.print(host);
+  Serial.print(':');
+  Serial.println(port);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed"); delay(1000); return;
+  }
+
+  // Get data from sensor
+  Serial.println("getting data from sensor");
+  setupi2c();
+  float temp = readi2c(0xF3);
+  float humid = readi2c(0xF5);
+  String message = "";
+  message = String(temp, 1) + " " + String(humid);
+
+
+  // This will send a string to the server
+  Serial.println("sending data to server");
+  if (client.connected()) { 
+    client.println(message);
+  }
+
+
+  // Close the connection
+  Serial.println();
+  Serial.println("closing connection");
+  client.stop();
+
+  ESP.deepSleep(SLEEPTIME); // execute once every 5 minutes, don't flood remote service
+}
